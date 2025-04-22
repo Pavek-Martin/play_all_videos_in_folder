598e4971faf049eac1770237420bc0a953865864
Martin pávek <martin_pavek@email.cz>
2025-04-22 09:42:57 +0200

    Delete mpv directory

--- mpv/play_video_dir_4..bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -File .\play_video_dir_4.ps1 "C:/Users/DELL/Videos/Denisa_videa/"
-pause
-@echo on
--- mpv/play_video_dir_5_args_1.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo "zadej cestu k video souborum "
-echo "napr. C:\Users\DELL\Videos\Baudisova"
-set /p cesta=""
-echo "cesta = %cesta%"
-pause
-powershell -File ./play_video_dir_5_args_1.ps1 %cesta%
-pause
-echo on
